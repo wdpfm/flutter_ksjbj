65577844a6ebcd79bdcab6ec7cc0d83965361618
wdpfm <16840517@qq.com>
2019-07-30 10:11:03 +0800

    水平布局、垂直布局、Container布局、GridView布局、ListView布局、Stack布局、Card布局、层叠定位、滚动布局

---
+++ README.md
@@ -0,0 +1,11 @@
+# 腾讯课堂亢少军老师的Flutter基础教程学习
+## 水平布局、垂直布局、Container布局、GridView布局、ListView布局、Stack布局、Card布局、层叠定位、滚动布局
+![](https://github.com/wdpfm/flutter_ksjbj/blob/master/sc/sp.png)
+![](https://github.com/wdpfm/flutter_ksjbj/blob/master/sc/cz.png)
+![](https://github.com/wdpfm/flutter_ksjbj/blob/master/sc/ct.png)
+![](https://github.com/wdpfm/flutter_ksjbj/blob/master/sc/grid.png)
+![](https://github.com/wdpfm/flutter_ksjbj/blob/master/sc/list.png)
+![](https://github.com/wdpfm/flutter_ksjbj/blob/master/sc/stack.png)
+![](https://github.com/wdpfm/flutter_ksjbj/blob/master/sc/card.png)
+![](https://github.com/wdpfm/flutter_ksjbj/blob/master/sc/stackpos.png)
+![](https://github.com/wdpfm/flutter_ksjbj/blob/master/sc/roll.png)
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,362 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(
       title: '布局示例',
-      home: new GridLayoutDemo(),
+      home: new RollLayoutDemo(),
+      //home: new PositionedLayoutDemo(),
+      //home: new CardLayoutDemo(),
+      //home: new StackLayoutDemo(),
+      //home: new ListLayoutDemo(),
+      //home: new GridLayoutDemo(),
       //home: new CTLayoutDemo(),
       //home: new CZLayoutDemo(),
       //home: new SPLayoutDemo(),
     ));
+
+class RollLayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('滚动布局示例'),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new Center(
+            child: new Text(
+              '\n哈哈哈',
+              style: new TextStyle(
+                fontSize: 30.0,
+              ),
+            ),
+          ),
+          new Center(
+            child: new Text(
+              '啦啦啦',
+              style: new TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+          new Center(
+            child: new Text(
+              '''
+              耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶
+              耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶
+              耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶耶
+              ''',
+              style: new TextStyle(
+                fontSize: 12.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PositionedLayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('层叠定位布局示例'),
+      ),
+      body: Center(
+        child: new Stack(
+          children: <Widget>[
+            new Image.asset('images/1.jpg'),
+            new Positioned(
+                top: 50.0,
+                left: 50.0,
+                child: new Text(
+                  '5555555',
+                  style: new TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                    fontFamily: 'serif',
+                    color: Colors.pinkAccent,
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardLayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var card = new SizedBox(
+      height: 250.0,
+      child: new Card(
+        child: new Column(
+          children: <Widget>[
+            new ListTile(
+              title: new Text(
+                '你你你你你你',
+                style: new TextStyle(
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              subtitle: new Text('强强强强强强'),
+              leading: new Icon(
+                Icons.account_box,
+                color: Colors.pinkAccent,
+              ),
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text(
+                '你你你你你你',
+                style: new TextStyle(
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              subtitle: new Text('强强强强强强'),
+              leading: new Icon(
+                Icons.account_box,
+                color: Colors.pinkAccent,
+              ),
+            ),
+            new Divider(),
+          ],
+        ),
+      ),
+    );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Card布局示例'),
+      ),
+      body: new Center(
+        child: card,
+      ),
+    );
+  }
+}
+
+class StackLayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var stack = new Stack(
+      alignment: const FractionalOffset(0.5, 0.9), //子元素相对排列位置
+      children: <Widget>[
+        new CircleAvatar(
+          backgroundImage: new AssetImage('images/1.jpg'),
+          radius: 100.0,
+        ),
+        new Container(
+          decoration: new BoxDecoration(
+            color: Colors.pinkAccent,
+          ),
+          child: new Text(
+            'hello baby',
+            style: new TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text('Stack布局示例'),
+      ),
+      body: new Center(
+        child: stack,
+      ),
+    );
+  }
+}
+
+class ListLayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> list = <Widget>[
+      new ListTile(
+        title: new Text(
+          ''
+          '哈哈哈哈',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text('啦啦啦啦啦'),
+        leading: new Icon(
+          Icons.wifi,
+          color: Colors.pinkAccent,
+        ),
+      ),
+      new ListTile(
+        title: new Text(
+          ''
+          '啊啊啊啊啊',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text(
+          '哇哇哇',
+          style: new TextStyle(color: Colors.pinkAccent),
+        ),
+        leading: new Icon(
+          Icons.wifi,
+          color: Colors.pink,
+        ),
+      ),
+      new ListTile(
+        title: new Text(
+          ''
+          '呀呀呀呀',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text('吱吱吱吱吱吱'),
+        leading: new Icon(
+          Icons.wifi_lock,
+          color: Colors.redAccent,
+        ),
+      ),
+      new ListTile(
+        title: new Text(
+          ''
+          '呀呀呀呀',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text('吱吱吱吱吱吱'),
+        leading: new Icon(
+          Icons.wifi_lock,
+          color: Colors.redAccent,
+        ),
+      ),
+      new ListTile(
+        title: new Text(
+          ''
+          '呀呀呀呀',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text('吱吱吱吱吱吱'),
+        leading: new Icon(
+          Icons.wifi_lock,
+          color: Colors.redAccent,
+        ),
+      ),
+      new ListTile(
+        title: new Text(
+          ''
+          '呀呀呀呀',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text('吱吱吱吱吱吱'),
+        leading: new Icon(
+          Icons.wifi_lock,
+          color: Colors.redAccent,
+        ),
+      ),
+      new ListTile(
+        title: new Text(
+          ''
+          '呀呀呀呀',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text('吱吱吱吱吱吱'),
+        leading: new Icon(
+          Icons.wifi_lock,
+          color: Colors.redAccent,
+        ),
+      ),
+      new ListTile(
+        title: new Text(
+          ''
+          '呀呀呀呀',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text('吱吱吱吱吱吱'),
+        leading: new Icon(
+          Icons.wifi_lock,
+          color: Colors.redAccent,
+        ),
+      ),
+      new ListTile(
+        title: new Text(
+          ''
+          '呀呀呀呀',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text('吱吱吱吱吱吱'),
+        leading: new Icon(
+          Icons.wifi_lock,
+          color: Colors.redAccent,
+        ),
+      ),
+      new ListTile(
+        title: new Text(
+          ''
+          '呀呀呀呀',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text('吱吱吱吱吱吱'),
+        leading: new Icon(
+          Icons.wifi_lock,
+          color: Colors.redAccent,
+        ),
+      ),
+      new ListTile(
+        title: new Text(
+          ''
+          '呀呀呀呀',
+          style: new TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20.0,
+          ),
+        ),
+        subtitle: new Text('吱吱吱吱吱吱'),
+        leading: new Icon(
+          Icons.wifi_lock,
+          color: Colors.redAccent,
+        ),
+      ),
+    ];
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('ListView布局示例'),
+      ),
+      body: new Center(
+        child: new ListView(
+          children: list,
+        ),
+      ),
+    );
+  }
+}
 
 class GridLayoutDemo extends StatelessWidget {
   @override
@@ -18,7 +369,8 @@ class GridLayoutDemo extends StatelessWidget {
                 child: new Image.asset(
                   'images/${index + 1}.jpg',
                   fit: BoxFit.cover,
-                  height: 60.0,),
+                  height: 60.0,
+                ),
               ));
     }
 
@@ -31,6 +383,7 @@ class GridLayoutDemo extends StatelessWidget {
         children: _buildGridTitleList(9),
       );
     }
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('GridView布局示例'),
